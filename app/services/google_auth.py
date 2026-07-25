fb7373dd185a5eb17e62b1fec4b205316b15ee38
esteven <ecalcinap@unsa.du.pe>
2026-07-25 14:12:23 -0500

    authenticacion excepcion

--- app/services/google_auth.py
+++ app/services/google_auth.py
@@ -1,4 +1,5 @@
 import requests
+from fastapi import HTTPException
 
 
 GOOGLE_TOKEN_INFO_URL = "https://oauth2.googleapis.com/tokeninfo"
@@ -6,15 +7,23 @@ GOOGLE_TOKEN_INFO_URL = "https://oauth2.googleapis.com/tokeninfo"
 
 def verify_google_token(token: str):
 
-    response = requests.get(
-        GOOGLE_TOKEN_INFO_URL,
-        params={"id_token": token}
-    )
+    try:
+        response = requests.get(
+            GOOGLE_TOKEN_INFO_URL,
+            params={"id_token": token}
+         )
+    except requests.RequestException:
+        raise HTTPException(
+            status_code=503,
+            detail="No se pudo conectar"
+        )
 
     if response.status_code != 200:
-        return {
-            "success": False,
-            "message": "Token inválido"
-        }
+        raise HTTPException(
+            status_code=response.status_code,
+            detail="Token invalido o expirado"
+        )
 
+
+        
     return response.json()
