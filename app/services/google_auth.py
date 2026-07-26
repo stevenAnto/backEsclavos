70ffef7c038f612dd2de9b16b3e4398fb59a5be2
esteven <ecalcinap@unsa.du.pe>
2026-07-26 07:57:55 -0500

    fixed httpExceptions

--- app/services/google_auth.py
+++ app/services/google_auth.py
@@ -1,29 +1,43 @@
+import jwt
 import requests
 from fastapi import HTTPException
+from datetime import datetime,timezone
 
 
 GOOGLE_TOKEN_INFO_URL = "https://oauth2.googleapis.com/tokeninfo"
 
 
 def verify_google_token(token: str):
-
     try:
-        response = requests.get(
-            GOOGLE_TOKEN_INFO_URL,
-            params={"id_token": token}
-         )
-    except requests.RequestException:
-        raise HTTPException(
-            status_code=503,
-            detail="No se pudo conectar"
+        payload=jwt.decode(
+            token,
+            options={"verify_signature": False}
         )
+    except jwt.DecodeError:
+        raise HTTPException(
+            status_code=400,
+            detail="Token invalido"
+        )
+    exp = payload.get("exp")
+
+    ahora = datetime.now(timezone.utc).timestamp()
+
+    if exp < ahora:
+        raise HTTPException(
+            status_code=401,
+            detail="Token expirado"
+        )
+
+    response = requests.get(
+    GOOGLE_TOKEN_INFO_URL,
+    params={"id_token": token}
+)
 
     if response.status_code != 200:
         raise HTTPException(
-            status_code=response.status_code,
-            detail="Token invalido o expirado"
-        )
-
+            status_code=400,
+            detail="Token inválido"
+         )
 
         
     return response.json()
